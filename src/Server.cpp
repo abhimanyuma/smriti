6eb19727ace1a4565ac25d7caf7250e91d834d4a
Abhimanyu Mongandambalath <abhi@manyu.in>
2025-07-07 19:59:03 +0100

    Add possible code to handle PING messages

--- src/Server.cpp
+++ src/Server.cpp
@@ -8,6 +8,8 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#include "handler.h"
+
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
@@ -50,10 +52,22 @@ int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
+  while (true) {
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    if (client_fd < 0) {
+      std::cerr << "Failed to accept client connection\n";
+      continue; // Continue to accept new connections
+    }
 
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
-  
+    std::cout << "Client connected\n";
+
+    handle_client(client_fd); // Call the function to handle the request
+    
+    close(client_fd);
+    std::cout << "Client disconnected\n";
+
+  }
+
   close(server_fd);
 
   return 0;
