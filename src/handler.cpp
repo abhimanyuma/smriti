20e0357b5eac9b3d295b7c54992a629c50df4b98
Abhimanyu Mongandambalath <abhi@manyu.in>
2025-07-07 20:19:51 +0100

    Update ping command to be simple

--- src/handler.cpp
+++ src/handler.cpp
@@ -8,47 +8,13 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int get_input(char* input_buffer, int client_fd) {
-    ssize_t bytes_received = recv(client_fd, input_buffer, sizeof(input_buffer) - 1, 0);
-    
-    if (bytes_received < 0) {
-        std::cerr << "Failed to receive data from client\n";
-        return -1;
-    } else if (bytes_received == 0) {
-        std::cout << "Client disconnected\n";
-        return 0; // Client disconnected
-    }
-
-    input_buffer[bytes_received] = '\0';
-}
-
-int handle_output(char* output_buffer, const char* input_buffer, int client_fd) {
-    // Process the input and prepare the output
-    if (std::strcmp(input_buffer, "PING") == 0) {
-        std::strcpy(output_buffer, "+PONG\r\n");
-    } else {
-        std::strcpy(output_buffer, "-ERR unknown command\r\n");
-    }
-    
-    return 1; // Successfully sent response
-}
 
 int handle_client(int client_fd) {
-    char input_buffer[1024];
-    
-    get_input(input_buffer, client_fd);
-    
-    char output_buffer[1024];
-    handle_output(output_buffer, input_buffer, client_fd);
-    
-    ssize_t bytes_sent = send(client_fd, output_buffer, std::strlen(output_buffer), 0);
-    if (bytes_sent < 0) {
-        std::cerr << "Failed to send data to client\n";
-        return -1; // Error in sending response
-    } else if (bytes_sent == 0) {
-        std::cout << "Client disconnected while sending response\n";
-        return 0; // Client disconnected
-    }
-    std::cout << "Response sent to client: " << output_buffer;
+
+    // For now we respond to all requests with a simple PONG response
+    // We cannot currently handle the full redis-cli
+
+    send(client_fd, "+PONG\r\n", 7, 0); // Send a simple PONG response
+    std::cout << "Handled client request, sent PONG response\n";
     return 1; // Successfully handled request
 }
